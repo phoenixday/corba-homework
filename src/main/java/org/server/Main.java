3e99e6fee6b41d3dfd210d4f14c541a3669bb5b6
“phoenixday” <xtsykyn@fi.muni.cz>
2023-03-13 08:35:46 +0100

    initial commit

---
+++ src/main/java/org/server/Main.java
@@ -0,0 +1,12 @@
+package org.server;
+
+// https://is.muni.cz/auth/el/1433/jaro2023/PA053/um/homework1.txt
+// https://www.youtube.com/watch?v=chsR860gbsk
+// https://wiki.archlinux.org/title/java#Switching_between_JVM
+// https://docs.oracle.com/javase/10/tools/idlj.htm#JSWOR711
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
